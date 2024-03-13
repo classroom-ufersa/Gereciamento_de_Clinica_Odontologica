afead7e27338eb563f9931d2ca9055301842332e
leticiavieirag <90807534+LeticiaVieirg@users.noreply.github.com>
2024-03-13 20:27:06 -0300

    Update consultorio.c

--- TADconsultorio/consultorio.c
+++ TADconsultorio/consultorio.c
@@ -1,1 +1,17 @@
+#inclide "configuracoes.h"
+#include "consultorio.h"
 
+struct consultorio {
+    int identificacao;
+    char especialidade[50];
+    char equipamentos_disponiveis[50];
+    Paciente* pacientes;
+};
+
+Consultorio adicionar_consultorio(Consultorio *c){
+
+}
+
+void remover_consultorio(Consultorio *c){
+  
+}
