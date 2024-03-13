1bf6457dfd70f167bb138598269fd769518d2c0c
leticiavieirag <90807534+LeticiaVieirg@users.noreply.github.com>
2024-03-13 20:29:41 -0300

    Update consultorio.h

--- TADconsultorio/consultorio.h
+++ TADconsultorio/consultorio.h
@@ -1,1 +1,12 @@
+#inclide "configuracoes.h"
 
+/*Renomeacao de um tipo estruturado para Consultorio*/
+typedef struct consultorio Consultorio;
+
+/*Funcao que adiciona um consultorio,
+  Recebe como paramentro um ponteiro*/
+Consultorio adicionar_consultorio(Consultorio *c);
+
+/*Funcao que remove um consultorio,
+  Recebe como parametro um ponteiro*/
+void remover_consultorio(Consultorio *c);
