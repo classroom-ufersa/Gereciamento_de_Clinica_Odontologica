230d298675a579f0a99220a8e2deaf555b656516
leticiavieirag <90807534+LeticiaVieirg@users.noreply.github.com>
2024-03-13 20:21:38 -0300

    Create paciente.h

---
+++ TADpaciente/paciente.h
@@ -0,0 +1,18 @@
+/*Renomeacao de um tipo estruturado*/
+typedef struct paciente Paciente;
+
+/*Funcao que adiciona um paciente,
+  Recebe como parametro um ponteiro*/
+Paciente adicionar_paciente(Paciente *p);
+
+/*Funcao que remove um paciente,
+  Recebe como parametro um ponteiro*/
+void remover_paciente(Paciente *p);
+
+/*Funcao que edita as informacoes de um paciente,
+  Recebe como parametros um ponteiro*/
+Paciente editar_paciente(Paciente *p, char[Max])
+
+/*Funcao que busca o paciente pelo nome,
+  Recebe como parametro um ponteiro*/
+void buscar_paciente(Paciente *p);
