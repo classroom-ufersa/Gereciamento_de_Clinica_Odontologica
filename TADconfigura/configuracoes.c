22d37d7b842d3eb0260920ac950fe533ae84eea8
leticiavieirag <90807534+LeticiaVieirg@users.noreply.github.com>
2024-03-13 20:35:58 -0300

    Update configuracoes.c

--- TADconfigura/configuracoes.c
+++ TADconfigura/configuracoes.c
@@ -1,1 +1,22 @@
+#include "configuracoes.h"
 
+void menu(void){
+    printf("Gerenciamento Clinica Odontologica.\n");
+    printf("1-Adicionar consultorio\n");
+    printf("2-Remover consultorio \n");
+    printf("3-Adicionar paciente\n");
+    printf("4-Remover paciente\n");
+    printf("5-Editar informacoes de paciente\n");
+    printf("6-Buscar paciente por nome\n");
+    printf("7-Listar consultorios e pacientes atendidos\n");
+    printf("8-Sair\n");
+
+}
+
+void limpa_buffer(void){
+
+}
+
+void listar(void){
+    
+}
