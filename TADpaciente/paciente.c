6d097ac846c58dbfab929fe3d0ed97d743cf8a86
leticiavieirag <90807534+LeticiaVieirg@users.noreply.github.com>
2024-03-13 20:05:42 -0300

    Create paciente.c

---
+++ TADpaciente/paciente.c
@@ -0,0 +1,27 @@
+#include 
+
+struct paciente{
+    char nome[Max];
+    int idade;
+    int situacao_saude;
+};
+
+Paciente adicionar_paciente(Paciente *p){
+    p=(Paciente*)malloc(sizeof(Paciente*));
+    if (a==NULL){
+        printf("Erro de alocação de memória");
+        exit(1);
+    }
+    return p;
+}
+
+void remover_paciente(Paciente *p){
+}
+
+Paciente editar_paciente(Paciente *p, char[Max]){
+}
+
+void buscar_paciente(Paciente *p){
+    
+}
+
