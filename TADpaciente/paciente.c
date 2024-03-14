2e7ab4914fbc796ac7be45cbbbb7627c30981469
Isabelly Lima <mslima5643@gmail.com>
2024-03-14 13:14:24 -0300

    update

--- TADpaciente/paciente.c
+++ TADpaciente/paciente.c
@@ -6,16 +6,40 @@ struct paciente{
     int situacao_saude;
 };
 
-Paciente adicionar_paciente(Paciente *p){
+Paciente coletar_dados_paciente(Paciente *p){
     p=(Paciente*)malloc(sizeof(Paciente*));
     if (a==NULL){
         printf("Erro de alocação de memória");
         exit(1);
     }
+    do{
+        qualquer=0; char  choose;
+   printf("Caso deseje cadastrar algum paciente digite \"S\"\n");
+    scanf(" %c", &choose);
+    choose = toupper(choose); 
+char escolha;
+    if (choose == 'S'){
+      p = (Paciente*)realloc(p, (Quantidade_pacientes + 1) * sizeof(Pacientes));
+
     return p;
 }
+printf("Digite o nome do cliente:\n");
+scanf("%s" p->nome);
+printf ("Digite a idade do paciente:\n");
+scanf("%d", p->idade);
+printf("Qual a situação da saúde, digite B para bom, R para ruim e M para medio:\n");
+scanf("%s", escolha);
+if (escolha== 'B' && escolha=='b')
+
+{
+    p->situacao_saude= "Boa";
+}
+
+
+Quantidade_pacientes++;
 
 void remover_paciente(Paciente *p){
+
 }
 
 Paciente editar_paciente(Paciente *p, char[Max]){
--- main.c
+++ main.c
@@ -1,1 +1,9 @@
+#include<stdio.h>
 
+
+
+p=(Paciente*)malloc(sizeof(Paciente*));
+    if (a==NULL){
+        printf("Erro de alocação de memória");
+        exit(1);
+    }
