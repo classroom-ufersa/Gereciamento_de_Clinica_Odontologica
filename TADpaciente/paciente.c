080b33aff22d215db537bbbd1fa695a6ed175ca8
Isabelly Lima <mslima5643@gmail.com>
2024-03-15 08:22:56 -0300

    correcaodeerros

--- TADpaciente/paciente.c
+++ TADpaciente/paciente.c
@@ -8,44 +8,53 @@ struct paciente{
 
 Paciente* coletar_dados_paciente(Paciente *p, int quantidade_pacientes){
     
-do{
-        int continuidade=0; 
-        char  choose;
-   printf("Caso deseje cadastrar algum paciente digite \"S\"\n");
-    scanf(" %c", &choose);
-    choose = toupper(choose); 
-char escolha;
-    if (choose == 'S'){
-      p = (Paciente*)realloc(p, (quantidade_pacientes + 1) * sizeof(Pacientes));
+Paciente* p = NULL;
+    int quantidade_pacientes = 0;
+    char choose;
+    int continuidade = 1;
 
-    return p;
+    while (continuidade) {
+        printf("Deseja adicionar um paciente? (S/N): ");
+        scanf(" %c", &choose); 
 
-printf("Digite o nome do cliente:\n");
-scanf("%s" p->nome);
-printf ("Digite a idade do paciente:\n");
-scanf("%d", p->idade);
-printf("Qual a situação da saúde, digite B para bom, R para ruim e M para medio:\n");
-scanf("%s", escolha);
-if (escolha == 'B') {
-                strcpy(p->situacao_saude, "Boa");
+        if (choose == 'S' || choose == 's') {
+            p = (Paciente*)realloc(p, (quantidade_pacientes + 1) * sizeof(Paciente));
+
+            printf("Digite o nome do cliente: ");
+            scanf("%s", p[quantidade_pacientes].nome);
+
+            printf("Digite a idade do paciente: ");
+            scanf("%d", &p[quantidade_pacientes].idade);
+
+            printf("Qual a situação da saúde (B para bom, R para ruim e M para médio): ");
+            scanf(" %s", escolha); 
+
+            if (escolha[0] == 'B') {
+                strcpy(p[quantidade_pacientes].situacao_saude, "Boa");
             }
-            else if (escolha == 'M') {
-                strcpy(p->situacao_saude, "Media");
+            else if (escolha[0] == 'M') {
+                strcpy(p[quantidade_pacientes].situacao_saude, "Média");
             }
-            else if (escolha == 'R') {
-                strcpy(p->situacao_saude, "Ruim");
+            else if (escolha[0] == 'R') {
+                strcpy(p[quantidade_pacientes].situacao_saude, "Ruim");
             }
             else {
-                printf("Escolha apenas as letras 'R', 'B' ou 'M':\n");
-                return NULL;
+                printf("Escolha apenas as letras 'R', 'B' ou 'M'.\n");
+                continue; 
             }
 
+            quantidade_pacientes++;
+        }
+        else if (choose != 's' && choose != 'S') {
+            printf("Digite novamente, apenas a letra 'S' ou 'N'.\n");
+        }
+    }
 
+    
 
-quantidade_pacientes++;
-continuidade=1
-    }}while(continuidade==1);
- 
+    return 0;
+}
+
  void escrever_pacientes(Paciente*, int quantidade_pacientes)
  void ler_pacientes(Paciente*p, int quantidade_pacientes)
 void remover_paciente(Paciente *p){
