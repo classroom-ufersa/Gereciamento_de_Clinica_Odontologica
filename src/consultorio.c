7411696f520c2905fd45f0b502bdb4a520714b13
leticiavieirag <90807534+LeticiaVieirg@users.noreply.github.com>
2024-03-23 15:18:59 -0300

    Update consultorio.c
    add da funcao ler consultorio, ordenar por id e buscar por id

--- src/consultorio.c
+++ src/consultorio.c
@@ -18,6 +18,7 @@ struct Listaconsultorio* lista_cria() {
     return NULL;
 }
 
+/*Tipo estruturado que cria e adiciona um novo consultorio a lista encadeada*/
 struct Listaconsultorio* listaconsultorio(Consultorio* consultorio, Listaconsultorio* Listaconsultorio) {
     struct Listaconsultorio* novo = (Listaconsultorio*)malloc(sizeof(Listaconsultorio));
     if (novo == NULL) {
@@ -30,22 +31,24 @@ struct Listaconsultorio* listaconsultorio(Consultorio* consultorio, Listaconsult
     return novo;
 }
 
+/*Tipo estruturado que insere o consultorio de forma ordenada por meio do id*/
 struct Consultorio* inserir_ordenado(Consultorio* inicio, Consultorio* novo) {
-    if (inicio == NULL || (novo->identificacao, inicio->identificacao) < 0) {
+    if (inicio == NULL || novo->identificacao < inicio->identificacao) {
         novo->proximo = inicio;
         return novo;
     }
     
     struct Consultorio* atual = inicio;
-    while (atual->proximo != NULL && (novo->identificacao, atual->proximo->identificacao) >= 0) {
+    while (atual->proximo != NULL && novo->identificacao >= atual->proximo->identificacao) {
         atual = atual->proximo;
-        
-    } //mudar depois porque tem que ser ordenado por id nao por nome.
+    } 
+    
     novo->proximo = atual->proximo;
     atual->proximo = novo;
     return inicio;
 }
 
+/*Tipo estruturado para coleta e armazenamento de dados do consultorio*/
 struct Listaconsultorio* coletar_dados_consultorio() {
     
     printf("Digite a identificacao do consultorio: ");
@@ -56,25 +59,35 @@ struct Listaconsultorio* coletar_dados_consultorio() {
 
     printf("Digite os equipamentos disponiveis: \n");
     scanf("%[^\n]" novo_consultorio->equipamentos_disponiveis);
-    }
+}
 
 void escrever_consultorio(Listaconsultorio *listaconsultorio) {
+    //texto
+}
+
+/*Funcao que percorre a lista e imprime na tela as informações de cada consultorio*/
+void ler_consultorio(Listaconsultorio *listaconsultorio) {
+    struct Listaconsultorio *atual = listaconsultorio;
     
+    while (atual != NULL) {
+        printf("Identificacao: %d\n", atual->consultorio->identificacao);
+        printf("Especialidade: %s\n", atual->consultorio->especialidade);
+        printf("Equipamentos Disponiveis: %s\n", atual->consultorio->equipamentos_disponiveis);
+        
+        atual = atual->next;
+    }
 }
-   
-void ler_consultorio(Listaconsultorio *listaconsultorio){
 
-}
-
+/*Fucao que remove consultorio a partir do seu id*/
 void remover_consultorio(Listaconsultorio *listaconsultorio) {
-    char nomeremover[200];
-    printf("Digite o nome do consultorio que deseja remover:\n");
-    scanf(" %[^\n]", nomeremover);
+    int id_remover;
+    printf("Digite a identificacao do consultorio que deseja remover:\n");
+    scanf("%d", &id_remover);
 
-    struct Listapaciente* atual = *listapaciente;
-    struct Listapaciente* anterior = NULL;
+    struct Listaconsultorio *atual = listaconsultorio;
+    struct Listaconsultorio *anterior = NULL;
 
-    while (atual != NULL && strcmp(atual->paciente->identificacao, nomeremover) != 0) {
+    while (atual != NULL && atual->consultorio->identificacao != id_remover) {
         anterior = atual;
         atual = atual->next;
     }
@@ -85,7 +98,7 @@ void remover_consultorio(Listaconsultorio *listaconsultorio) {
     }
 
     if (anterior == NULL) {
-        *listaconsultorio = atual->next;
+        listaconsultorio = atual->next;
     } else {
         anterior->next = atual->next;
     }
@@ -94,38 +107,35 @@ void remover_consultorio(Listaconsultorio *listaconsultorio) {
     free(atual);
 }
 
+
 Consultorio adicionar_consultorio(Consultorio *c){
 
 }
 
+/*Funcao que verifica se a lista esta vazia*/
 void verificar_lista_vazia(Listaconsultorio *lista_consultorio){
     if(lista_consultorio==NULL){
-    return 1;}
-return 0;
+        return 1;
+    }
+    return 0;
 }
 
-void remover_consultorio(Consultorio *c){
-  
-}
-
+/*Funcao que buscar por um paciente específico dentro de uma lista de consultórios*/
 void buscar_por_paciente(Listaconsultorio* lista_consultorio, char* nome_paciente) {
     if (lista_consultorio == NULL) {
         printf("Lista de consultórios vazia.\n");
         return;
     }
-
     Listaconsultorio* consultorio_atual = lista_consultorio;
     while (consultorio_atual != NULL) {
-     
         Listapacientes* paciente_atual = consultorio_atual->lista_pacientes;
         while (paciente_atual != NULL) {
             if (strcmp(paciente_atual->paciente->nome, nome_paciente) == 0) {
                 printf("Paciente encontrado: %s\n", paciente_atual->paciente->nome);
-               
                 return;
             }
             paciente_atual = paciente_atual->next;
         }
-        
         consultorio_atual = consultorio_atual->next_consultorio;
     }
+}
