f00dbb3c58f0e4c7e826c1a66ffdaebe74d94b2b
Isabelly Lima <mslima5643@gmail.com>
2024-03-24 19:38:24 -0300

    mudanca no.h do arquivo de teste

--- teste/paciente.h
+++ teste/paciente.h
@@ -1,17 +1,17 @@
 #ifndef PACIENTE_H
 #define PACIENTE_H
 
-struct Paciente {
+typedef struct Paciente {
     char nome[100];
     int idade;
     char situacao_saude[100];
     struct Paciente* proximo;
-};
+}Paciente;
 
-struct ListaPaciente {
+typedef struct ListaPaciente {
     struct Paciente* paciente;
     struct ListaPaciente* proximo;
-};
+}ListaPaciente;
 struct Paciente* coletar_dados_paciente();
 struct ListaPaciente* inserir_paciente_ordenado(struct ListaPaciente* lista_pacientes, struct Paciente*novo_paciente);
 void buscar_por_nome_paciente(struct ListaPaciente* lista);
