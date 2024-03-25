e090609b49cdeb2acaa4518731b4a010511a2bad
leticiavieirag <90807534+LeticiaVieirg@users.noreply.github.com>
2024-03-25 20:00:40 -0300

    Update consultorio.h

--- teste/consultorio.h
+++ teste/consultorio.h
@@ -8,15 +8,20 @@ typedef struct Consultorio {
     struct Paciente* paciente;
     struct Consultorio* proximo;
     struct Lista_geral*listageral;
-
 } Consultorio;
+
 typedef struct listageral{
     struct Lista_geral*proximo;
     struct Paciente*paciente_geral;
+} Lista_geral;
 
-    
-}Lista_geral;
-
+/*Funcao que adiciona pacinte a lista geral*/
 Lista_geral* adicionar_paciente_geral(Lista_geral* lista_geral, Paciente* novo_paciente);
+
+/*Funcao que adicina um consultorio*/
 Consultorio* adicionar_consultorio(Consultorio* lista_consultorios);
+
+/*Funcao que remove um consultorio por meio do id informado*/
+Consultorio*remover_consultorio_por_id(Consultorio*lista_consultorios, int id);
+
 #endif
