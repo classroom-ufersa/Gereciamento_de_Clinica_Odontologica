5cd4b4417cad5172095f99303f3c11d4a478e383
leticiavieirag <90807534+LeticiaVieirg@users.noreply.github.com>
2024-03-29 19:09:44 -0300

    Update consultorio.h

--- src/consultorio.h
+++ src/consultorio.h
@@ -1,19 +1,47 @@
 #ifndef CONSULTORIO_H
 #define CONSULTORIO_H
-  #include"paciente.h"
-typedef struct consultorio Consultorio;
+#include"paciente.h"
 
+typedef struct consultorio {
+    int identificacao;
+    char especialidade[400];
+    char equipamentos_disponiveis[100];
 
-typedef struct listaconsultorio Listaconsultorio;
+    struct Paciente* paciente;
+    struct Consultorio* proximo;
+    struct Lista_geral* listageral;
+} Consultorio; 
 
-struct Listaconsultorio* lista_cria();
- Listaconsultorio* adicionar_consultorio(Consultorio* consultorio, Listaconsultorio* lista_consultorios);
+/*Funcao que verifica se a lista esta vazia,
+  Recebe como parametros um ponteiro de consultorio*/
+int verificar_lista(Consultorio*consultorio_aux);
 
-Consultorio* coletar_Dados();
-void escrever_consultorio(struct Listaconsultorio* listaconsultorio);
-void ler_consultorio(struct Listaconsultorio *listaconsultorio);
-void remover_consultorio(Listaconsultorio **listaconsultorio);
-void verificar_lista_vazia(struct Listaconsultorio* lista_consultorio);
+/*Funcao que adiciona um consultorio a lista de consultorios,
+  Recebe como parametros um ponteiro de consultorio*/
+Consultorio* adicionar_consultorio(Consultorio* lista_consultorios);
 
-#endif
+/*Funcao que remove um consultorio pelo ID informado,
+  Recebe como parametro um ponteiro de consultorio*/
+Consultorio* remover_consultorio_por_id(Consultorio* lista_consultorios);
 
+/*Funcao que imprime os consultorios disponiveis,
+  Recebe como parametro um ponteiro de consultorio*/
+void imprimir_consultorios_Disponiveis(Consultorio* lista);
+
+/*Funcao que adiciona um paciente ao consultorio informado, 
+  Recebe como parametro um ponteiro de consultorio*/
+void adicionar_paciente_por_id(Consultorio *lista_consultorios);
+
+/*Funcao que busca o paciente pelo nome, 
+  Recebe como parametros um ponteiro de consultorio e o nome*/
+Consultorio* buscar_paciente_por_nome(Consultorio* lista_consultorios, char* nome);
+
+/*Funncao que salva o consultorio no arquivo de texto,
+  Recebe como parametro um ponteiro para consultorios*/
+void salvar_consultorios_em_arquivo(Consultorio* lista_consultorios);
+
+/*Funcao que edita os dados do paciente existente na lista,
+  Recebe com parametros um ponteiro de consultorio e o nome do paciente para editar*/
+Consultorio* editar_paciente(Consultorio* lista, char* nome_editar);
+
+#endif /* CONSULTORIO_H */
