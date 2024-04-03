b57047b0fe05fae72d26ca39c5ecfa8b717de176
Isabelly Lima <134648243+isabellylimals@users.noreply.github.com>
2024-04-03 14:55:44 -0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -12,22 +12,20 @@ int main() {
     char opcao;
 
     ler_arquivo_e_inserir_lista(&lista_consultorios,&lista_paciente);
-
-    
     do {
         printf("========= CHAR OF SMILE DENTAL CLINIC ==========\n");
         printf("Seja Bem Vindo! Segue o menu da clinica: \n");
-        printf("[1]-Adicionar consultorio\n");
-        printf("[2]-Remover consultorio \n");
-        printf("[3]-Adicionar paciente\n");
-        printf("[4]-Remover paciente\n");
-        printf("[5]-Editar informacoes de paciente\n");
-        printf("[6]-Buscar paciente por nome\n");
-        printf("[7]-Listar consultorios \n");
-        printf("[8]-Listar pacientes atendidos\n");
-        printf("[9]- Sair\n");
-         printf("==============================================\n");
-         printf("Escolha uma das opcoes acima:\n");
+        printf("[1] Adicionar consultorio\n");
+        printf("[2] Remover consultorio \n");
+        printf("[3] Adicionar paciente\n");
+        printf("[4] Remover paciente\n");
+        printf("[5] Editar informacoes de paciente\n");
+        printf("[6] Buscar paciente por nome\n");
+        printf("[7] Listar consultorios \n");
+        printf("[8] Listar pacientes atendidos\n");
+        printf("[9] Sair\n");
+        printf("==============================================\n");
+        printf("Escolha uma das opcoes acima:\n");
 
         scanf(" %c", &opcao);
     
@@ -55,6 +53,7 @@ int main() {
                 char nome_editar[500];
                 char dg_string[100];
                 int digito_unico;
+
                 printf("Digite o nome do paciente que deseja editar:\n");
                 scanf(" %[^\n]", nome_editar);
                 tratamento_de_palavras(nome_editar);
@@ -67,7 +66,7 @@ int main() {
                 editar_paciente(lista_consultorios, nome_editar,digito_unico);
                 break;
             }
-                case '6': {
+            case '6': {
                 char nome_buscar[500];
                 char dg_string[100];
                 int digito_unico;
@@ -80,7 +79,7 @@ int main() {
                 tratamento_de_numero(dg_string);
                 digito_unico=atoi(dg_string);
                 lista_paciente = buscar_paciente_por_nome(lista_consultorios, nome_buscar, digito_unico);
-                }
+            }
             case '7': {
                 imprimir_consultorios_Disponiveis(lista_consultorios);
                 break;
@@ -92,7 +91,7 @@ int main() {
                 break;
             }
             case '9': {
-                printf("Saindo....\n");
+                printf("Saindo...\n");
                 break;
             }
         
@@ -104,7 +103,7 @@ int main() {
 
     } while (opcao != '9');
   
-  //Liberar memoria
+    //Liberar memoria
     free(lista_consultorios);
     free(lista_atendidos);
     free(lista_paciente);
